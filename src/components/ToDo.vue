43a61e9af0cce37cb1b51fd0fdda26640b353c02
jakub-maziarz <jakub.maziarz@outlook.com>
2023-03-04 18:22:19 +0100

    divided ToDo component into smaller components

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,8 +1,21 @@
 <template>
 <div class="container-xl">
-    
+    <div class="row">
+        <Today></Today>
+        <FilterTasks></FilterTasks>
+    </div>
+    <TodoList></TodoList>
+    <NewTask></NewTask>
+    <FinishTasks></FinishTasks>
+    <Footer></Footer>
 </div>
 </template>
 
 <script setup>
+import Today from './ToDo/Today.vue';
+import FilterTasks from './ToDo/FilterTasks.vue';
+import TodoList from './ToDo/TodoList.vue';
+import NewTask from './ToDo/NewTask.vue';
+import FinishTasks from './ToDo/FinishTasks.vue';
+import Footer from './ToDo/Footer.vue';
 </script>
