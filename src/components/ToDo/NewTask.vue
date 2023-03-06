63dd7539930ed1de72cc630f9148f691c315b3d9
jakub-maziarz <jakub.maziarz@outlook.com>
2023-03-06 18:20:17 +0100

    edit NewTask component

--- src/components/ToDo/NewTask.vue
+++ src/components/ToDo/NewTask.vue
@@ -1,1 +1,51 @@
-<template></template>
+<template>
+    <div class="new-task d-flex align-items-center px-2 mt-4">
+        <div class="new-task__icon border border-2 me-4">
+            <font-awesome-icon icon="fa-solid fa-plus" size="2xs" />
+        </div>
+        <div class="new-task__input flex-grow-1 fs-5">
+            <input type="text" name="new-task" id="new-task" placeholder="Dodaj nowe zadanie...">
+        </div>
+    </div>
+</template>
+<style>
+.new-task__icon {
+    border-radius: 50%;
+    padding: 0px 7px;
+    color: var(--task-add-primary-color);
+    border-color: var(--task-add-primary-color) !important;
+    background-color: var(--task-add-secondary-color);
+    cursor: pointer;
+}
+
+.new-task__icon:hover,
+.new-task__icon:active {
+    color: var(--task-add-secondary-color);
+    background-color: var(--task-add-primary-color);
+}
+
+.new-task__input input {
+    width: 100%;
+    border: none;
+    outline: none;
+    color: var(--task-text-color);
+}
+
+.new-task__input input::placeholder {
+    /* Chrome, Firefox, Opera, Safari 10.1+ */
+    color: var(--task-add-primary-color) !important;
+    opacity: 1;
+    /* Firefox */
+}
+
+.new-task__input input:-ms-input-placeholder {
+    /* Internet Explorer 10-11 */
+    color: var(--task-add-primary-color) !important;
+}
+
+.new-task__input input::-ms-input-placeholder {
+    /* Microsoft Edge */
+    color: var(--task-add-primary-color) !important;
+}
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import "bootstrap/dist/css/bootstrap.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faEllipsis, faCircle as faCircleSolid, faCircleXmark as  faCircleXmarkSolid } from "@fortawesome/free-solid-svg-icons";
-import { faCircle as faCircleRegular, faCircleXmark as  faCircleXmarkRegular} from "@fortawesome/free-regular-svg-icons";
+import { faEllipsis, faCircle as faCircleSolid, faCircleXmark as  faCircleXmarkSolid, faCirclePlus as faCirclePlusSolid, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faCircle as faCircleRegular, faCircleXmark as  faCircleXmarkRegular } from "@fortawesome/free-regular-svg-icons";
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-library.add(faEllipsis, faCircleRegular, faCircleSolid, faCircleXmarkRegular, faCircleXmarkSolid);
+library.add(faEllipsis, faCircleRegular, faCircleSolid, faCircleXmarkRegular, faCircleXmarkSolid, faCirclePlusSolid, faPlus);
 
 createApp(App)
     .component("font-awesome-icon", FontAwesomeIcon)
